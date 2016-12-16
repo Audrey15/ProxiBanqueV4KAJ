ab962979f81e562bd9e8b379ef4a31c1cd8f50c4
audrey15 <audrey.gambade@hotmail.fr>
2016-12-16 16:31:42 +0100

    service3

--- src/main/java/org/formation/service/IServiceConseiller.java
+++ src/main/java/org/formation/service/IServiceConseiller.java
@@ -2,7 +2,7 @@ package org.formation.service;
 
 import java.util.List;
 
-import org.formation.model.Client;
+
 import org.formation.model.Conseiller;
 
 public interface IServiceConseiller {
@@ -13,7 +13,7 @@ public interface IServiceConseiller {
 
 	public void deleteConseillerById(Conseiller cons);
 
-	public Client findConseillerById(Conseiller cons);
+	public Conseiller findConseillerById(Conseiller cons);
 
 	public List<Conseiller> findAllConseiller();
 
