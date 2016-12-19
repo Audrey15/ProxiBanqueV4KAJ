a5b0b6156d319b608a0b585f59cc8801c8f910ff
fumfu <true_fumfu@hotmail.fr>
2016-12-19 12:31:43 +0100

    navbar
    menu

--- src/main/java/org/formation/controller/ClientControllerImpl.java
+++ src/main/java/org/formation/controller/ClientControllerImpl.java
@@ -25,12 +25,12 @@ public class ClientControllerImpl implements IClientController, Serializable {
 
 	@Resource
 	IServiceClient serviceClient;
-	
+
 	private List<Client> listeClients = new ArrayList<>();
 	private List<Client> listeClientsByConseiller = new ArrayList<>();
-	
+
 	private Client client;
-	
+
 	public Client getClient() {
 		return client;
 	}
@@ -62,43 +62,42 @@ public class ClientControllerImpl implements IClientController, Serializable {
 		listeClients = serviceClient.findAllClients();
 		return listeClients;
 	}
-	
+
 	@Override
 	public List<Client> findAllClientsByConseiller(Conseiller conseiller) throws Exception {
 		listeClientsByConseiller = serviceClient.findAllClientsByConseiller(conseiller);
 		return listeClientsByConseiller;
 	}
 
-	public void loadClient(Long idCli) throws Exception{
+	public void loadClient(Long idCli) throws Exception {
 		Client client = serviceClient.findClientById(idCli);
 		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
 		Map<String, Object> sessionMap = externalContext.getSessionMap();
 		sessionMap.put("cl", client);
 	}
-	
+
 	@Override
-	public String loadClientForUpdate(Long idCli) throws Exception{
+	public String loadClientForUpdate(Long idCli) throws Exception {
 		loadClient(idCli);
 		return "modifierclient";
 	}
 
 	@Override
-	public String loadClientForInfo(Long idCli) throws Exception{
+	public String loadClientForInfo(Long idCli) throws Exception {
 		loadClient(idCli);
 		return "infoclient";
 	}
 
 	@Override
-	public String loadClientForComptes(Long idCli) throws Exception{
+	public String loadClientForComptes(Long idCli) throws Exception {
 		loadClient(idCli);
 		return "/views/compte/listecompteclient.xhtml";
 	}
 
 	@Override
-	public String loadClientForAjoutCompte(Long idCli) throws Exception{
+	public String loadClientForAjoutCompte(Long idCli) throws Exception {
 		loadClient(idCli);
 		return "ajoutercompte";
 	}
-	
 
 }
