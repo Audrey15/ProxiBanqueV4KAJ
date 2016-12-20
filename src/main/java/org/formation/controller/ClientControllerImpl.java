a9e8956edbe00fdcec3e0c900966f8635ce219e6
Kevin <kevin.lebreton.res@gmail.com>
2016-12-20 10:32:43 +0100

    correction
    correction

--- src/main/java/org/formation/controller/ClientControllerImpl.java
+++ src/main/java/org/formation/controller/ClientControllerImpl.java
@@ -14,6 +14,7 @@ import org.formation.model.Client;
 import org.formation.model.CompteBancaire;
 import org.formation.model.Conseiller;
 import org.formation.service.IServiceClient;
+import org.formation.service.IServiceCompte;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.context.annotation.ApplicationScope;
 
@@ -24,6 +25,9 @@ public class ClientControllerImpl implements IClientController, Serializable {
 
 	@Resource
 	IServiceClient serviceClient;
+	
+	@Resource
+	IServiceCompte serviceCompte;
 
 	private List<Client> listeClients = new ArrayList<>();
 	private List<Client> listeClientsByConseiller = new ArrayList<>();
@@ -41,6 +45,7 @@ public class ClientControllerImpl implements IClientController, Serializable {
 	@Override
 	public String createClient(Client client, CompteBancaire compte) throws Exception {
 		serviceClient.createClient(client);
+		serviceCompte.createCompteForClient(compte, client);
 		return "/views/client/listeclients";
 	}
 
